20657f8c23e57e3aadd0fd9979bd49f4a27015c1
augustine d. nguyen <augustine.d.nguyen@outlook.com>
2020-11-20 15:32:13 +0700

    initial

---
+++ src/main/java/com/tinyolo/cxml/parsing/demo/ParsingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tinyolo.cxml.parsing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyolo/cxml/parsing/demo/ParsingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyolo.cxml.parsing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
